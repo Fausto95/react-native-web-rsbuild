2f1a066ff4c8086c83d7498ee500e4b5159bafcf
Fausto95 <faustino.kialungila@gmail.com>
2024-05-09 13:43:56 +0200

    fix: modules resolution for workspace

--- apps/rnw/rsbuild.config.ts
+++ apps/rnw/rsbuild.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+// import { pluginBabel } from '@rsbuild/plugin-babel';
 import path from "node:path";
 
 const packagesDir = path.resolve(__dirname, "../../libs");
 
 export default defineConfig({
   plugins: [pluginReact()],
+  // plugins: [pluginReact(), pluginBabel(
+  //   {babelLoaderOptions: (config, { addPlugins }) => {
+  //     addPlugins([
+  //       ["react-native-web", { commonjs: true }]
+  //     ]);
+  //     return config;
+  //   }}
+  // )],
   source: {
     alias: {
-      "react-native": "react-native-web",
-      "rn-core": "../../libs/core",
-      // "rn-design-system": "../../libs/design-system",
+      'react-native': 'react-native-web',
     },
     include: [
       {
